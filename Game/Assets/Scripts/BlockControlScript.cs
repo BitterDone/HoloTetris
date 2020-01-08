6bcb01b129a37bb99b928935955735c182ba75f3
WLSmith9386 <wlsmith9386@yahoo.com>
2020-01-07 18:45:35 -0600

    Hololens select functionality
    Added Functionality to move and rotate active block based on select gesture

--- Game/Assets/Scripts/BlockControlScript.cs
+++ Game/Assets/Scripts/BlockControlScript.cs
@@ -4,93 +4,111 @@ using UnityEngine;
 
 public class BlockControlScript : MonoBehaviour
 {
-    private BlockSpawner spawner;
-    private float previousTime;
+    //Moved everything to BlockSpawner so it's easier to control on the Hololens. Keeping all code in this script for backup purposes
 
-    public float fallTime = .8f;
+    //    private BlockSpawner spawner;
+    //    private float previousTime;
 
-    public static int height = 20;
-    public static int width = 10;
-    public static Transform[,] grid = new Transform[width, height];
+    //    public float fallTime = .8f;
 
-	// Use this for initialization
-	void Start ()
-    {
-        spawner = GameObject.Find("Spawner").GetComponent<BlockSpawner>();	
-	}
+    //    public static int height = 20;
+    //    public static int width = 10;
+    //    public static Transform[,] grid = new Transform[width, height];
 
-    // Update is called once per frame
-    void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.LeftArrow))
-        {
-            transform.position -= new Vector3(1, 0, 0);
-            if (!ValidMove())
-            {
-                transform.position += new Vector3(1, 0, 0);
-            }
-        }
-        else if (Input.GetKeyDown(KeyCode.RightArrow))
-        {
-            transform.position += new Vector3(1, 0, 0);
-            if (!ValidMove())
-            {
-                transform.position -= new Vector3(1, 0, 0);
-            }
-        }
-        else if (Input.GetKeyDown(KeyCode.UpArrow))
-        {
-            transform.eulerAngles += new Vector3(0, 0, 90);
+    //    // Use this for initialization
+    //    void Start ()
+    //    {
+    //        spawner = GameObject.Find("Spawner").GetComponent<BlockSpawner>();	
+    //	}
 
-            if (!ValidMove())
-            {
-                transform.eulerAngles -= new Vector3(0, 0, 90);
-            }
+    //    // Update is called once per frame
+    //    void Update()
+    //    {
+    //        if (Input.GetKeyDown(KeyCode.LeftArrow))
+    //        {
+    //            MoveLeft();
+    //        }
+    //        else if (Input.GetKeyDown(KeyCode.RightArrow))
+    //        {
+    //            MoveRight();
+    //        }
+    //        else if (Input.GetKeyDown(KeyCode.UpArrow))
+    //        {
+    //            RotatePiece();
 
-        }
+    //        }
 
-        if (Time.time - previousTime > (Input.GetKey(KeyCode.DownArrow) ? fallTime / 10 : fallTime))
-        {
-            transform.position -= new Vector3(0, 1, 0);
-            if (!ValidMove())
-            {
-                transform.position += new Vector3(0, 1, 0);
-                AddToGrid();
-                this.enabled = false;
-                spawner.SpawnNewBlock();
-            }
-            previousTime = Time.time;
-        }
-    }
+    //        if (Time.time - previousTime > (Input.GetKey(KeyCode.DownArrow) ? fallTime / 10 : fallTime))
+    //        {
+    //            transform.position -= new Vector3(0, 1, 0);
+    //            if (!ValidMove())
+    //            {
+    //                transform.position += new Vector3(0, 1, 0);
+    //                AddToGrid();
+    //                this.enabled = false;
+    //                spawner.SpawnNewBlock();
+    //            }
+    //            previousTime = Time.time;
+    //        }
+    //    }
 
-    void AddToGrid()
-    {
-        foreach (Transform c in transform)
-        {
-            int roundX = Mathf.RoundToInt(c.transform.position.x);
-            int roundY = Mathf.RoundToInt(c.transform.position.y);
+    //    public void RotatePiece()
+    //    {
+    //        transform.eulerAngles += new Vector3(0, 0, 90);
 
-            grid[roundX, roundY] = c;
-        }
-    }
+    //        if (!ValidMove())
+    //        {
+    //            transform.eulerAngles -= new Vector3(0, 0, 90);
+    //        }
+    //    }
 
-    bool ValidMove()
-    {
-        foreach (Transform c in transform)
-        {
-            int roundX = Mathf.RoundToInt(c.transform.position.x);
-            int roundY = Mathf.RoundToInt(c.transform.position.y);
+    //    public void MoveRight()
+    //    {
+    //        transform.position += new Vector3(1, 0, 0);
+    //        if (!ValidMove())
+    //        {
+    //            transform.position -= new Vector3(1, 0, 0);
+    //        }
+    //    }
 
-            if (roundX < 0 || roundX >= width || roundY < 0)
-            {
-                return false;
-            }
-            if (grid[roundX, roundY] != null)
-            {
-                return false;
-            }
-        }
+    //    public void MoveLeft()
+    //    {
+    //        transform.position -= new Vector3(1, 0, 0);
+    //        if (!ValidMove())
+    //        {
+    //            transform.position += new Vector3(1, 0, 0);
+    //        }
+    //    }
 
-        return true;
-    }
+    //    void AddToGrid()
+    //    {
+    //        foreach (Transform c in transform)
+    //        {
+    //            int roundX = Mathf.RoundToInt(c.transform.position.x);
+    //            int roundY = Mathf.RoundToInt(c.transform.position.y);
+
+    //            grid[roundX, roundY] = c;
+    //        }
+    //    }
+
+    //    bool ValidMove()
+    //    {
+    //        foreach (Transform c in transform)
+    //        {
+    //            int roundX = Mathf.RoundToInt(c.transform.position.x);
+    //            int roundY = Mathf.RoundToInt(c.transform.position.y);
+
+    //            if (roundX < 0 || roundX >= width || roundY < 0)
+    //            {
+    //                return false;
+    //            }
+    //            if (grid[roundX, roundY] != null)
+    //            {
+    //                return false;
+    //            }
+    //        }
+
+    //        return true;
+    //    }
+    //}
 }
--- Game/Assets/Scripts/BlockSpawner.cs
+++ Game/Assets/Scripts/BlockSpawner.cs
@@ -5,16 +5,113 @@ using UnityEngine;
 public class BlockSpawner : MonoBehaviour {
 
     public GameObject[] blocks;
+    public GameObject activeBlock;
+    // Use this for initialization
 
-	// Use this for initialization
-	void Start ()
+    private float previousTime;
+
+    public float fallTime = .8f;
+
+    public static int height = 20;
+    public static int width = 10;
+    public static Transform[,] grid = new Transform[width, height];
+    void Start ()
     {
         SpawnNewBlock();
 	}
 
     public void SpawnNewBlock()
     {
-        Instantiate(blocks[Random.Range(0, blocks.Length)], transform.position, Quaternion.identity);
+        activeBlock = Instantiate(blocks[Random.Range(0, blocks.Length)], transform.position, Quaternion.identity);
     }
-	
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            MoveLeft();
+        }
+        else if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            MoveRight();
+        }
+        else if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            RotatePiece();
+
+        }
+
+        if (Time.time - previousTime > (Input.GetKey(KeyCode.DownArrow) ? fallTime / 10 : fallTime))
+        {
+            activeBlock.transform.position -= new Vector3(0, 1, 0);
+            if (!ValidMove())
+            {
+                activeBlock.transform.position += new Vector3(0, 1, 0);
+                AddToGrid();
+                this.enabled = false;
+                SpawnNewBlock();
+            }
+            previousTime = Time.time;
+        }
+    }
+
+    public void RotatePiece()
+    {
+        activeBlock.transform.eulerAngles += new Vector3(0, 0, 90);
+
+        if (!ValidMove())
+        {
+            activeBlock.transform.eulerAngles -= new Vector3(0, 0, 90);
+        }
+    }
+
+    public void MoveRight()
+    {
+        activeBlock.transform.position += new Vector3(1, 0, 0);
+        if (!ValidMove())
+        {
+            activeBlock.transform.position -= new Vector3(1, 0, 0);
+        }
+    }
+
+    public void MoveLeft()
+    {
+        activeBlock.transform.position -= new Vector3(1, 0, 0);
+        if (!ValidMove())
+        {
+            activeBlock.transform.position += new Vector3(1, 0, 0);
+        }
+    }
+
+    bool ValidMove()
+    {
+        foreach (Transform c in activeBlock.transform)
+        {
+            int roundX = Mathf.RoundToInt(c.transform.position.x);
+            int roundY = Mathf.RoundToInt(c.transform.position.y);
+
+            if (roundX < 0 || roundX >= width || roundY < 0)
+            {
+                return false;
+            }
+            if (grid[roundX, roundY] != null)
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    void AddToGrid()
+    {
+        foreach (Transform c in activeBlock.transform)
+        {
+            int roundX = Mathf.RoundToInt(c.transform.position.x);
+            int roundY = Mathf.RoundToInt(c.transform.position.y);
+
+            grid[roundX, roundY] = c;
+        }
+    }
+
 }
